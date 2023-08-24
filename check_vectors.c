58b05e10c86593b55863ac7343fa868bf69335dd
Yassein Idris <yasinaltyb@yahoo.com>
2023-08-24 08:42:53 +0400

    enhanced check for values

--- check_vectors.c
+++ check_vectors.c
@@ -6,7 +6,7 @@
 /*   By: ymohamed <ymohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 23:07:41 by ymohamed          #+#    #+#             */
-/*   Updated: 2023/08/20 23:27:12 by ymohamed         ###   ########.fr       */
+/*   Updated: 2023/08/24 05:31:10 by ymohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,42 +19,85 @@ static int	is_normalized(t_tuple v)
 	return (1);
 }
 
-static void	check_plane_vector(t_plane *plane, t_ranger *alive)
+static int	errocheck_plane(t_plane *plane)
 {
 	if (!is_normalized(plane->normal_v))
 	{
 		ft_printf("Error\nPlane vector is not normalized\n");
-		free_objects(alive);
-		exit (0);
+		return (1);
 	}
+	if (plane->color.red < 0 || plane->color.red > 255
+		|| plane->color.green < 0 || plane->color.green > 255
+		|| plane->color.blue < 0 || plane->color.blue > 255)
+	{
+		ft_printf("Error\nPlane color is not in range [0, 255]\n");
+		return (1);
+	}
+	return (0);
 }
 
-static void	check_cylinder_vector(t_cylndr *cylinder, t_ranger *alive)
+static int	errocheck_cylindr(t_cylndr *cylinder)
 {
 	if (!is_normalized(cylinder->vec))
 	{
 		ft_printf("Error\nCylinder vector is not normalized\n");
-		free_objects(alive);
-		exit (0);
+		return (1);
 	}
+	if (cylinder->color.red < 0 || cylinder->color.red > 255
+		|| cylinder->color.green < 0 || cylinder->color.green > 255
+		|| cylinder->color.blue < 0 || cylinder->color.blue > 255)
+	{
+		ft_printf("Error\nCylinder color is not in range [0, 255]\n");
+		return (1);
+	}
+	if (cylinder->rad <= 0 || cylinder->height <= 0)
+	{
+		ft_printf("Error\nCylinder radius or height is negative or zero\n");
+		return (1);
+	}
+	return (0);
+}
+
+static int	errocheck_sphere(t_sphere *sphere)
+{
+	if (sphere->color.red < 0 || sphere->color.red > 255
+		|| sphere->color.green < 0 || sphere->color.green > 255
+		|| sphere->color.blue < 0 || sphere->color.blue > 255)
+	{
+		ft_printf("Error\nSphere color is not in range [0, 255]\n");
+		return (1);
+	}
+	if (sphere->rad <= 0)
+	{
+		ft_printf("Error\nSphere radius is negative or zero\n");
+		return (1);
+	}
+	return (0);
 }
 
 void	check_vectors(t_ranger *alive)
 {
 	int	i;
+	int	err;
 
 	i = -1;
+	err = 0;
 	if (!is_normalized(alive->cam.look_forward))
-	{
-		ft_printf("Error\nCamera look vector is not normalized\n");
-		free_objects(alive);
-		exit (0);
-	}
-	while (++i < alive->no_of_object)
+		err = ft_printf("Error\nCamera look vector is not normalized\n");
+	if (alive->cam.field_of_view <= 0 || alive->cam.field_of_view >= 180)
+		err = ft_printf("Error\nCamera fov is not in range (0, 180)\n");
+	while (++i < alive->no_of_object && !err)
 	{
 		if (alive->objcs[i].obj_type == plane)
-			check_plane_vector((t_plane *)alive->objcs[i].the_obj, alive);
+			err = errocheck_plane((t_plane *)alive->objcs[i].the_obj);
 		else if (alive->objcs[i].obj_type == cylinder)
-			check_cylinder_vector((t_cylndr *)alive->objcs[i].the_obj, alive);
+			err = errocheck_cylindr((t_cylndr *)alive->objcs[i].the_obj);
+		else if (alive->objcs[i].obj_type == sphere)
+			err = errocheck_sphere((t_sphere *)alive->objcs[i].the_obj);
+	}
+	if (err)
+	{
+		free_objects(alive);
+		exit (0);
 	}
 }
--- pre_parse.c
+++ pre_parse.c
@@ -6,11 +6,88 @@
 /*   By: ymohamed <ymohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 22:19:03 by ymohamed          #+#    #+#             */
-/*   Updated: 2023/08/20 22:36:33 by ymohamed         ###   ########.fr       */
+/*   Updated: 2023/08/24 08:28:30 by ymohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "raytrace.h"
+
+static void	free_line(char **line)
+{
+	free(*line);
+	*line = NULL;
+}
+
+static int	dot_errors(char *line, int i)
+{
+	if (!ft_isdigit(line[i - 1])
+		|| !ft_isdigit(line[i + 1]))
+		return (1);
+	i++;
+	while (line[i] && ft_isdigit(line[i]))
+		i++;
+	if (line[i] == '.')
+		return (1);
+	return (0);
+}
+
+static int	check_line(char *line)
+{
+	int	i;
+	int	err;
+
+	i = 0;
+	err = 0;
+	while (line[i] && (line[i] == ' ' || line[i] == '\t'))
+		i++;
+	if (!line[i] || line[i] == '\n' || line[i] == '#')
+		return (0);
+	if (!ft_isdigit(line[i]) && !ft_strchr("\n\t ,.-+ACLcypls", line[i]))
+		err++;
+	i = 1;
+	while (line[i] && !err)
+	{
+		if (!ft_isdigit(line[i]) && !ft_strchr("\n\t ,.-+ACLcypls", line[i]))
+			err++;
+		else if (line[i] == '.')
+			err = dot_errors(line, i);
+		else if (line[i] == ',' && (!ft_isdigit(line[i - 1])
+				|| (!ft_isdigit(line[i + 1]) && line[i + 1] != '-'
+					&& line[i + 1] != '+')))
+			err++;
+		i++;
+	}
+	return (err);
+}
+
+static void	contents_check(char *file)
+{
+	int		fd;
+	char	*line;
+	int		err;
+
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+	{
+		ft_printf("Error\nIvalid Dirctory");
+		exit (0);
+	}
+	err = 0;
+	line = get_next_line(fd);
+	while (line)
+	{
+		err += check_line(line);
+		if (line)
+			free_line(&line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	if (err)
+	{
+		ft_printf("Error invalid format\n");
+		exit (0);
+	}
+}
 
 void	exam_the_map_file(char *file, char *extention)
 {
@@ -37,4 +114,5 @@ void	exam_the_map_file(char *file, char *extention)
 		ft_printf("Error\nIvalid extention\n");
 		exit (0);
 	}
+	contents_check(file);
 }
